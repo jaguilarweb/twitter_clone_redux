c8975a177a98d11ad32f022c879c14645c9de1f1
Jenny Aguilar <contact@jaguilarweb.com>
2021-10-18 22:00:52 -0300

    fix: Fix a Invalid hook call error message updating react and react-dom packages

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,12 +3,12 @@ import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
 import { setAuthedUser } from '../actions/authedUser'
 
-const AUTHED_ID = 'tylermcginnins'
+const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData(){
   return (dispatch) => {
     return getInitialData()
-    .then(( users, tweets) => {
+    .then(( {users, tweets}) => {
       dispatch(receiveUsers(users))
       dispatch(receiveTweets(tweets))
       dispatch(setAuthedUser(AUTHED_ID))
